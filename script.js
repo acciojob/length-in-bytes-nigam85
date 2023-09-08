adaa542c71ebfbe923ff23ed921a031f5e150967
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-08 11:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const byteSize = (str) => {
-  // write your code here
-};
+// const byteSize = (str) => {
+//   // write your code here
+	
+// };
 
+// // Do not change the code below
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
 
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+function byteSize(str) {
+  // Create a Blob object from the input string
+  const blob = new Blob([str], { type: 'text/plain' });
+
+  // Get the size of the Blob in bytes
+  return blob.size;
+}
